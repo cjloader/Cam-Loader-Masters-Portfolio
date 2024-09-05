23a612861804fb082215c5ef26bdfea9661bc081
Cam J. Loader <cjloader@loaderusa.net>
2024-09-05 12:56:50 -0400

    new hw

---
+++ Applied-Algorithms/assign2-LinkedList.py
@@ -0,0 +1,17 @@
+from asyncio.windows_events import NULL
+
+
+class HWLinkedList:
+    class Node:
+        def __init__(self, data):
+            self.data = data
+            self.next = None
+        
+       
+        def HWLinkedList():
+            self.head = NULL
+        def toString(self):
+            if self.head == NULL:
+                return ""
+            return self.head
+            
